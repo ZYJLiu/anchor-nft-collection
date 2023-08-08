aced436b38480d25d8040b8cab1272fdf30889a5
John <johnzhengyuliu@gmail.com>
2023-08-08 18:44:34 -0500

    mint nft in collection

--- tests/anchor-nft-collection.ts
+++ tests/anchor-nft-collection.ts
@@ -96,4 +96,168 @@ describe("anchor-nft-collection", () => {
     assert.isTrue(metadata[0].data.creators[0].verified);
     assert.isTrue(metadata[0].collectionDetails.__kind === "V1");
   });
+
+  it("create nft in collection", async () => {
+    const mint = anchor.web3.Keypair.generate();
+
+    const metadataPDA = await metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: mint.publicKey });
+
+    const masterEditionPDA = await metaplex
+      .nfts()
+      .pdas()
+      .masterEdition({ mint: mint.publicKey });
+
+    const tokenAccount = await spl.getAssociatedTokenAddress(
+      mint.publicKey,
+      wallet.publicKey
+    );
+
+    const collectionMetadataPDA = await metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: collectionPDA });
+
+    const collectionMasterEditionPDA = await metaplex
+      .nfts()
+      .pdas()
+      .masterEdition({ mint: collectionPDA });
+
+    const modifyComputeUnits =
+      anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({
+        units: 300_000,
+      });
+
+    const tx = await program.methods
+      .createNftInCollection(
+        testMetadata.uri,
+        testMetadata.name,
+        testMetadata.symbol
+      )
+      .accounts({
+        user: wallet.publicKey,
+        collectionMint: collectionPDA,
+        collectionMetadataAccount: collectionMetadataPDA,
+        collectionMasterEdition: collectionMasterEditionPDA,
+        nftMint: mint.publicKey,
+        metadataAccount: metadataPDA,
+        masterEdition: masterEditionPDA,
+        tokenAccount: tokenAccount,
+        tokenMetadataProgram: METADATA_PROGRAM_ID,
+      })
+      .transaction();
+
+    const transferTransaction = new anchor.web3.Transaction().add(
+      modifyComputeUnits,
+      tx
+    );
+
+    const txSig = await anchor.web3.sendAndConfirmTransaction(
+      connection,
+      transferTransaction,
+      [wallet.payer, mint],
+      { skipPreflight: true }
+    );
+
+    // check metadata account has expected data
+    const accInfo = await connection.getAccountInfo(metadataPDA);
+    const metadata = Metadata.deserialize(accInfo.data, 0);
+
+    assert.ok(
+      metadata[0].data.uri.startsWith(testMetadata.uri),
+      "URI in metadata does not start with expected URI"
+    );
+    assert.ok(
+      metadata[0].data.name.startsWith(testMetadata.name),
+      "Name in metadata does not start with expected name"
+    );
+    assert.ok(
+      metadata[0].data.symbol.startsWith(testMetadata.symbol),
+      "Symbol in metadata does not start with expected symbol"
+    );
+  });
+
+  it("create nft in collection", async () => {
+    const mint = anchor.web3.Keypair.generate();
+
+    const metadataPDA = await metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: mint.publicKey });
+
+    const masterEditionPDA = await metaplex
+      .nfts()
+      .pdas()
+      .masterEdition({ mint: mint.publicKey });
+
+    const tokenAccount = await spl.getAssociatedTokenAddress(
+      mint.publicKey,
+      wallet.publicKey
+    );
+
+    const collectionMetadataPDA = await metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: collectionPDA });
+
+    const collectionMasterEditionPDA = await metaplex
+      .nfts()
+      .pdas()
+      .masterEdition({ mint: collectionPDA });
+
+    const modifyComputeUnits =
+      anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({
+        units: 300_000,
+      });
+
+    const tx = await program.methods
+      .createNftInCollection(
+        testMetadata.uri,
+        testMetadata.name,
+        testMetadata.symbol
+      )
+      .accounts({
+        user: wallet.publicKey,
+        collectionMint: collectionPDA,
+        collectionMetadataAccount: collectionMetadataPDA,
+        collectionMasterEdition: collectionMasterEditionPDA,
+        nftMint: mint.publicKey,
+        metadataAccount: metadataPDA,
+        masterEdition: masterEditionPDA,
+        tokenAccount: tokenAccount,
+        tokenMetadataProgram: METADATA_PROGRAM_ID,
+      })
+      .transaction();
+
+    const transferTransaction = new anchor.web3.Transaction().add(
+      modifyComputeUnits,
+      tx
+    );
+
+    const txSig = await anchor.web3.sendAndConfirmTransaction(
+      connection,
+      transferTransaction,
+      [wallet.payer, mint],
+      { skipPreflight: true }
+    );
+
+    // check metadata account has expected data
+    const accInfo = await connection.getAccountInfo(metadataPDA);
+    const metadata = Metadata.deserialize(accInfo.data, 0);
+
+    assert.ok(
+      metadata[0].data.uri.startsWith(testMetadata.uri),
+      "URI in metadata does not start with expected URI"
+    );
+    assert.ok(
+      metadata[0].data.name.startsWith(testMetadata.name),
+      "Name in metadata does not start with expected name"
+    );
+    assert.ok(
+      metadata[0].data.symbol.startsWith(testMetadata.symbol),
+      "Symbol in metadata does not start with expected symbol"
+    );
+  });
 });
